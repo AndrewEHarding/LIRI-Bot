47b3cedfc508b9abae0675b241ba6b602a9dd7e2
Andrew Harding <dreweharding@gmail.com>
2018-08-23 02:11:32 -0400

    added keys, hopefully all npm installs, updated readme

--- code/liri.js
+++ code/liri.js
@@ -1,3 +1,6 @@
 require("dotenv").config();
-var request = require('request');
-var Spotify = require('node-spotify-api');
+var keys = require("keys");
+var request = require("request");
+var Spotify = require("node-spotify-api");
+var moment = require("moment");
+var fs = require("fs");
