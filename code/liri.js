67f3a470afa5b21429d09e5ab623f04a322453e6
Andrew Harding <dreweharding@gmail.com>
2018-08-23 23:34:36 -0400

    attempt number two, node mudules weren't working

--- code/liri.js
+++
@@ -1,6 +0,0 @@
-require("dotenv").config();
-var keys = require("keys");
-var request = require("request");
-var Spotify = require("node-spotify-api");
-var moment = require("moment");
-var fs = require("fs");
